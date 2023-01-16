a35c0b8e09ec89887ee8f659200724a62663a2ac
Joey Blankendaal <JoeyBlankendaal@users.noreply.github.com>
2023-01-16 19:21:51 +0100

    Add target group sidebar

--- public/index.php
+++ public/index.php
@@ -3,50 +3,79 @@ require_once('../private/header.php');
 
 $days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 $hours = get_hours_range();
-?>
-<header>
-    <h1><?= date('F Y'); ?></h1>
-</header>
 
-<table>
-    <thead>
-        <tr>
-            <th></th>
-            
+// TODO: replace placeholder target groups with database
+$target_groups = ['Business Development', 'Customer Support', 'Research & Development', 'Sales & Marketing'];
+?>
+
+<div class="flex-x">
+    <div class="flex-y width-25">
+        <header>
+            <h2>Target groups</h2>
+        </header>
+        
+        <form class="flex-y">
             <?php
-            foreach ($days as $day)
+            foreach ($target_groups as $target_group)
             {
             ?>
-                <th><?= $day; ?></th>
+                <div>
+                    <input type="checkbox" checked disabled>
+                    <label><?= $target_group; ?></label>
+                </div>
             <?php
             }
             ?>
-        </tr>
-    </thead>
-    
-    <tbody>
-        <?php
-        foreach ($hours as $hour)
-        {
-        ?>
-            <tr>
-                <th><?= $hour; ?></th>
-                
+            
+            <a class="button" href="#">Log in to filter</a>
+        </form>
+    </div>
+
+    <div class="flex-y width-75">
+        <header>
+            <h1><?= date('F Y'); ?></h1>
+        </header>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    
+                    <?php
+                    foreach ($days as $day)
+                    {
+                    ?>
+                        <th><?= $day; ?></th>
+                    <?php
+                    }
+                    ?>
+                </tr>
+            </thead>
+            
+            <tbody>
                 <?php
-                for ($i = 1; $i <= 7; $i++)
+                foreach ($hours as $hour)
                 {
                 ?>
-                    <td><!-- TODO: fill with calendar items --></td>
+                    <tr>
+                        <th><?= $hour; ?></th>
+                        
+                        <?php
+                        for ($i = 1; $i <= 7; $i++)
+                        {
+                        ?>
+                            <td><!-- TODO: fill with calendar items --></td>
+                        <?php
+                        }
+                        ?>
+                    </tr>
                 <?php
                 }
                 ?>
-            </tr>
-        <?php
-        }
-        ?>
-    </tbody>
-</table>
-
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <div class="flex-x">
     <section class="card width-50">
